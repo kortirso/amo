5e78c305923f7e3b65be82cd6740ddb12dc9017b
kortirso <kortirso@gmail.com>
2018-02-21 15:08:27 +0700

    fix service

--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class FormsController < Api::V1:BaseController
+    class FormsController < Api::V1::BaseController
       def demo
         demo = DemoCreationService.new.call(demo_params)
         render json: demo, status: 200
