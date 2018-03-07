ff6b6e33cf7202fd1c583c73d9bfc70187f832f1
kortirso <kortirso@gmail.com>
2018-03-07 11:44:57 +0700

    add auth method for amo service

--- app/services/demo_creation_service.rb
+++ app/services/demo_creation_service.rb
@@ -3,9 +3,9 @@ require 'httparty'
 # Service for creating DemoForms
 class DemoCreationService
   include HTTParty
-  base_uri 'https://workandplay.amocrm.ru/api/v2/incoming_leads'
+  base_uri 'https://workandplay.amocrm.ru'
 
-  attr_reader :login, :api_key
+  attr_reader :login, :api_key, :cookies
 
   def initialize
     config = Config.current
@@ -13,16 +13,24 @@ class DemoCreationService
     @api_key = config.api_key
   end
 
+  def auth
+    body = {'USER_LOGIN':login,'USER_HASH':api_key}
+
+    response = self.class.post('/private/api/auth.php?type=json', body: body)
+    @cookies = response.headers['set-cookie']
+    response.parsed_response
+  end
+
   def get_leads
-    response = self.class.get("?login=#{login}&api_key=#{api_key}")
+    response = self.class.get("/api/v2/incoming_leads?login=#{login}&api_key=#{api_key}")
     JSON.parse(response.parsed_response)
   end
 
   def create_lead(args = {})
     body = {"add":[{"source_name":"http://workandplay.ru","source_uid":"1","created_at":Time.now.to_i,"incoming_entities":{"leads":[{"name":'userio'}]},"incoming_lead_info":{"form_id":"1","form_page":"http://workandplay.ru","ip":"localhost","service_code":"1"}}]}
-    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Cookie': cookies}
 
-    response = self.class.post("/form?login=#{login}&api_key=#{api_key}", body: body, headers: headers)
+    response = self.class.post("/api/v2/incoming_leads/form?login=#{login}&api_key=#{api_key}", body: body, headers: headers)
     JSON.parse(response.parsed_response)
   end
 end
